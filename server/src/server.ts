eadd416f602d0e3f2a23773d9426f7abdfcfdc18
Lucas Assunção Costa <lucascosta28246@gmail.com>
2020-06-07 21:58:30 -0300

    gitignore files

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,4 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
+
 app.listen(route);
